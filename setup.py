18268dd6fa6b97608cbeb4a204960dfec898594d
dario <dario.klepoch@freenet.de>
2021-01-03 23:10:59 +0100

    package setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="etl_data_reader-CaptainDario",
+    version="1.0",
+    author="CaptainDario",
+    author_email="daapplab@gmail.com",
+    description="A python package for conveniently handling the ETL data set",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CaptainDario/ETL_data_reader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
